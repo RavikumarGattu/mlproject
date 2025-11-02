6554944dd0d8a8fe14b29cb1044696a457cd1cbc
RavikumarGattu <ravikumargattu2022@gmail.com>
2025-11-01 20:50:24 -0400

    Deleted duplicate functions

--- src/utils.py
+++ src/utils.py
@@ -72,14 +72,3 @@ def evaluate_models(X_train,y_train,X_test,y_test,models:dict,model_params:dict)
 
     except Exception as e:
         raise CustomException(e,sys)
-
-def load_object(file_path:str)->object:
-    '''
-    This function is used to load the object from the file path
-    '''
-    try:
-        with open(file_path,'rb') as file_obj:
-            return dill.load(file_obj)
-
-    except Exception as e:
-        raise CustomException(e,sys)
